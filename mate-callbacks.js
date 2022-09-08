36bc9968a9a70260f41c3594eebd0fd0b60a190e
Nimrodrive5 <marcko_r@hotmail.com>
2022-09-07 23:25:28 -0500

    Callback hell

---
+++ mate-callbacks.js
@@ -0,0 +1,9 @@
+let production = () => {
+    setTimeout (() =>{
+        console.log('Order received. Starting production...');
+        setTimeout (() =>{
+             console.log('Calentar el agua en la pava (no hervir');
+    },5000);
+},8000);
+
+}
